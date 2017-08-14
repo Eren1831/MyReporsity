9476b7debdc9db74d2689653bc9d79d16355b713
ErenDenek <erenwtp09@gmail.com>
2017-08-14 15:23:43 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ var veri;
 var all = {
 
 
-	valueTemp    : [],
+	valueCabin    : [],
 	valueMotorRight  : [],
 	valueMotorLeft : [],
 	valueHor     : [],
@@ -36,63 +36,65 @@ port.on('data',function (val) {
    veri = val.split("|");
   if(veri.length > 1)
   {
-  	all["valueTemp" ]["inTemp"] =veri[1];
-  	all["valueTemp" ]["outTemp"] =veri[2];
-  	
-        all["valueMotorLeft" ]["motorLeftTemp"] =veri[3];
-  	all["valueMotorLeft"]["LeftMotorSpeed"]   =veri[4];
-  	all["valueMotorLeft"]["MotorDriver1Input"]  =veri[5];
-  	all["valueMotorLeft"]["MotorDriver1OUTPUT"] =veri[6];
-  	all["valueMotorLeft"]["MotorDriver2INPUT"]  =veri[7];
-  	all["valueMotorLeft"]["MotorDriver2OUTPUT"] =veri[8];
-  	
-        all["valueMotorRight" ]["motorRightTemp"] =veri[9];
-  	all["valueMotorRight"]["RightMotorSpeed"] =veri[10];
-  	all["valueMotorRight"]["MotorDriver1INPUT"]  =veri[11];
-  	all["valueMotorRight"]["MotorDriver1OUTPUT"] =veri[12];
-  	all["valueMotorRight"]["MotorDriver2INPUT"]  =veri[13];
-  	all["valueMotorRight"]["MotorDriver2OUTPUT"] =veri[14];
-	
-	all["valueHor"]["status"]=veri[15];
-	all["valueHor"]["ambientTemp"]=veri[16];
-	all["valueHor"]["stackVoltage"]=veri[17];
-	all["valueHor"]["H2Voltage"]=veri[18];
-	all["valueHor"]["stackTemp"]=veri[19];
-	all["valueHor"]["current"]=veri[20];
-	all["valueHor"]["batteryVoltage"]=veri[21];
-        all["valueHor" ]["h2Temp"] =veri[22];
+  	all["valueCabin" ]["inTemp"] =veri[1];
+  	all["valueCabin" ]["outTemp"] =veri[2];
+  	all["valueCabin"]["solKapı"]  = veri[3];
+  	all["valueCabin"]["sagKapı"]  = veri[4];
+  	all["valueCabin"]["flasher"]  = veri[5];
+  	all["valueCabin"]["tavan"]    =veri[6];
+  	all["valueCabin"]["h2Sens"]   =veri[7];
 
-	all["valueBattery"]["cell0"]=veri[23];
-	all["valueBattery"]["cell1"]=veri[24];
-	all["valueBattery"]["cell2"]=veri[25];
-	all["valueBattery"]["cell3"]=veri[26];
-	all["valueBattery"]["cell4"]=veri[27];
-	all["valueBattery"]["cell5"]=veri[28];
-	all["valueBattery"]["cell6"]=veri[29];
-	all["valueBattery"]["cell7"]=veri[30];
-	all["valueBattery"]["cell8"]=veri[31];
-	all["valueBattery"]["cell9"]=veri[32];
-	all["valueBattery"]["cell10"]=veri[33];
-	all["valueBattery"]["cell11"]=veri[34];
-	all["valueBattery"]["cell12"]=veri[35];
-	all["valueBattery"]["cell13"]=veri[36];
-	all["valueBattery"]["cell14"]=veri[37];
-	all["valueBattery"]["temp1"]=veri[38];
-	all["valueBattery"]["temp2"]=veri[39];
-        all["valueBattery" ]["batteryTemp"] =veri[40];
-	all["valueBattery"]["current"]=veri[41];
+    all["valueMotorLeft" ]["motorLeftTemp"] =veri[8];
+  	all["valueMotorLeft"]["LeftMotorSpeed"]   =veri[9];
+  	all["valueMotorLeft"]["MotorDriver1Input"]  =veri[10];
+  	all["valueMotorLeft"]["MotorDriver1OUTPUT"] =veri[11];
+  	all["valueMotorLeft"]["MotorDriver2INPUT"]  =veri[12];
+  	all["valueMotorLeft"]["MotorDriver2OUTPUT"] =veri[13];
+  	
+    all["valueMotorRight" ]["motorRightTemp"] =veri[14];
+  	all["valueMotorRight"]["RightMotorSpeed"] =veri[15];
+  	all["valueMotorRight"]["MotorDriver1INPUT"]  =veri[16];
+  	all["valueMotorRight"]["MotorDriver1OUTPUT"] =veri[17];
+  	all["valueMotorRight"]["MotorDriver2INPUT"]  =veri[18];
+  	all["valueMotorRight"]["MotorDriver2OUTPUT"] =veri[19];
 	
-	all["valueLight"]["flasher"]=veri[42];
-	all["valueLight"]["kısa"]=veri[43];
-	all["valueLight"]["uzun"]=veri[44];
-	all["valueLight"]["sag"]=veri[45];
-	all["valueLight"]["sol"]=veri[46];
-	all["valueLight"]["neoSol"]=veri[47];
-	all["valueLight"]["neoSag"]=veri[48];
-        all["valueLight"]["silecek"]=veri[49];
-	all["valueLight"]["tavan"]=veri[50];
-	all["valueLİght"]["solKapı"]=veri[51];
-	all["valueLight"]["sagKapı"]=veri[52];
+	all["valueHor"]["status"]=veri[20];
+	all["valueHor"]["ambientTemp"]=veri[21];
+	all["valueHor"]["stackVoltage"]=veri[22];
+	all["valueHor"]["H2Voltage"]=veri[23];
+	all["valueHor"]["stackTemp"]=veri[24];
+	all["valueHor"]["current"]=veri[25];
+	all["valueHor"]["batteryVoltage"]=veri[26];
+    all["valueHor" ]["h2Temp"] =veri[27];
+
+	all["valueBattery"]["cell0"]=veri[28];
+	all["valueBattery"]["cell1"]=veri[29];
+	all["valueBattery"]["cell2"]=veri[30];
+	all["valueBattery"]["cell3"]=veri[31];
+	all["valueBattery"]["cell4"]=veri[32];
+	all["valueBattery"]["cell5"]=veri[33];
+	all["valueBattery"]["cell6"]=veri[34];
+	all["valueBattery"]["cell7"]=veri[35];
+	all["valueBattery"]["cell8"]=veri[36];
+	all["valueBattery"]["cell9"]=veri[37];
+	all["valueBattery"]["cell10"]=veri[38];
+	all["valueBattery"]["cell11"]=veri[39];
+	all["valueBattery"]["cell12"]=veri[40];
+	all["valueBattery"]["cell13"]=veri[41];
+	all["valueBattery"]["cell14"]=veri[42];
+	all["valueBattery"]["temp1"]=veri[43];
+	all["valueBattery"]["temp2"]=veri[44];
+    all["valueBattery" ]["batteryTemp"] =veri[45];
+	all["valueBattery"]["current"]=veri[46];
+	
+	all["valueLight"]["kısa"]=veri[47];
+	all["valueLight"]["uzun"]=veri[48];
+	all["valueLight"]["sag"]=veri[49];
+	all["valueLight"]["sol"]=veri[50];
+	all["valueLight"]["neoSol"]=veri[51];
+	all["valueLight"]["neoSag"]=veri[52];
+    all["valueLight"]["silecek"]=veri[53];
+
   	for(var i=0;i<=40;i++)
   	{
   		console.log(veri[i]);
@@ -113,7 +115,7 @@ var config = {
 
 var dataRefHor = firebase.database().ref('/***HORİZON***');
 var dataRefBattery = firebase.database().ref('***BATTERY***');
-var dataRefTemp = firebase.database().ref('/***TEMP***');
+var dataRefCabin = firebase.database().ref('/***CABİN***');
 var dataRefLeftMotor = firebase.database().ref('/***LEFTMOTOR***');
 var dataRefRightMotor=firebase.database().ref('/***RİGHTMOTOR***');
 var dataRefLight=firebase.database().ref('/****LİGHT***');
@@ -123,11 +125,17 @@ var dataRefLight=firebase.database().ref('/****LİGHT***');
 setTimeout(function(){
 setInterval(function (){
 
-	dataRefTemp.set({
+	dataRefCabin.set({
 
-		temp1  : all["valueTemp" ]["inTemp"],
-		temp2 : all["valueTemp" ]["outTemp"],
-    });
+		inTemp  : all["valueCabin" ]["inTemp"],
+		outTemp : all["valueCabin" ]["outTemp"],
+		SolKapı : all["valueCabin" ]["solKapı"],
+		SagKapı : all["valueCabin" ]["sagKapı"],
+		Flasher :all["valueCabin"]["flasher"],
+		Tavan   : all["valueCabin" ]["tavan"],
+		h2Sens  : all["valueCabin"]["h2Sens"],
+
+		    });
 
 	dataRefBattery.set({
 		cell1 : all["valueBattery"]["cell0"],
@@ -165,35 +173,31 @@ setInterval(function (){
 	dataRefMotorRight.set({
 		 
 		RightMotorSpeed :all["valueSpeed"]["RightMotor"],
-                MotorDriver2INPUT :all["valueCurrent"]["MotorDriver2INPUT"],
+        MotorDriver2INPUT :all["valueCurrent"]["MotorDriver2INPUT"],
 		MotorDriver2OUTPUT :all["valueCurrent"]["MotorDriver2OUTPUT"],
-                MotorDriverVoltage_2_INPUT :all["valueVoltage"]["MotorDriver2INPUT"] ,
+        MotorDriverVoltage_2_INPUT :all["valueVoltage"]["MotorDriver2INPUT"] ,
 		MotorDriverVoltage_2_OUTPUT :all["valueVoltage"]["MotorDriver2OUTPUT"],
-		MotorRight : all["valueTemp" ]["motorRightTemp"]
+		MotorRight : all["valueTemp" ]["motorRightTemp"],
 	
 	});
 
 	dataRefMotorLeft.set({
-                LeftMotorSpeed :all["valueSpeed"]["LefMotor"],
+        LeftMotorSpeed :all["valueSpeed"]["LefMotor"],
 		MotorDriver1INPUT :all["valueCurrent"]["MotorDriver1Input"],
 		MotorDriver1OUTPUT :all["valueCurrent"]["MotorDriver1OUTPUT"],
-                MotorDriverVoltage_1_INPUT :all["valueVoltage"]["MotorDriver1INPUT"] ,
+        MotorDriverVoltage_1_INPUT :all["valueVoltage"]["MotorDriver1INPUT"] ,
 		MotorDriverVoltage_1_OUTPUT :all["valueVoltage"]["MotorDriver1OUTPUT"],
-                MotorLeftTemp : all["valueTemp" ]["motorLeftTemp"],
+        MotorLeftTemp : all["valueTemp" ]["motorLeftTemp"],
 		
 	});
         dataRefLight.set({
-                Flasher :all["valueLight"]["flasher"],
 		Kısa :all["valueLight"]["kısa"],
 		Uzun :all["valueLight"]["uzun"],
-                Sag :all["valueLight"]["sag"] ,
+        Sag :all["valueLight"]["sag"] ,
 		Sol :all["valueLight"]["sol"],
-                NeoSag : all["valueTemp" ]["neoSag"],
-                NeoSol : all["valueTemp" ]["neoSol"],
-		Silecek : all["valueTemp" ]["motorLeftTemp"],
-		Tavan   : all["valueTemp" ]["silecek"],
-		SolKapı : all["valueTemp" ]["solKapı"],
-		SagKapı : all["valueTemp" ]["sagKapı"],
+        NeoSag : all["valueLight" ]["neoSag"],
+        NeoSol : all["valueLight" ]["neoSol"],
+		Silecek : all["valueLight" ]["silecek"],
 	});
 	
 }, 1000);
